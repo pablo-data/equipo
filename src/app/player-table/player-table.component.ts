f1db220c8843478deb31415a1b1524a95100a91c
Pablo Acuña <pablo.acu.ramirez@gmail.com>
2020-08-01 17:00:51 -0400

    feat(app): delete player

--- src/app/player-table/player-table.component.ts
+++ src/app/player-table/player-table.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Player } from '../interfaces/player';
 import { PlayerService } from '../services/player.service';
+import { TeamService } from '../services/team.service';
+import {take} from 'rxjs/operators';
 
 @Component({
   selector: 'app-player-table',
@@ -12,7 +14,7 @@ export class PlayerTableComponent implements OnInit {
   public players$: Observable<Player[]>;
   public selectedPlayer: Player;
   public showModal = false;
-  constructor(private playerService: PlayerService) { }
+  constructor(private playerService: PlayerService, private teamService: TeamService) { }
 
   ngOnInit(): void {
     this.players$ = this.playerService.getPlayers();
@@ -33,6 +35,19 @@ export class PlayerTableComponent implements OnInit {
     setTimeout(() => {
       window.location.replace('#open-modal');
     });
+  }
+
+  deletePlayer(player: Player){
+    this.teamService.getTeams().pipe(take(1)).subscribe(teams => {
+      const moddifiedPlayers = teams[0].players ? teams[0].players.filter((p: any) => p.key !== player.$key) : teams[0].players;
+      const formattedTeam = {
+        ...teams[0],
+        players: [...moddifiedPlayers]
+      };
+      this.playerService.deletePlayer(player.$key);
+      this.teamService.editTeam(formattedTeam);
+    });
+
   }
 
   closeDialog() {
